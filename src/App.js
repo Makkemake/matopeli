62c01dd50296710ec66985c942e13217e720128f
Makkemake <ww@w>
2021-02-09 18:18:18 +0200

    w

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import SnakeBoard from './SnakeBoard.js';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         Matopeli
       </header>
+      <SnakeBoard />
     </div>
   );
 }
