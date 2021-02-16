3246a7ac00c0eed5b4e3c614dca2e1b7e73b2f1e
Makkemake <ww@w>
2021-02-16 18:10:23 +0200

    w

--- src/SnakeBoard.js
+++ src/SnakeBoard.js
@@ -1,30 +1,30 @@
-
-import React, {useState} from 'react'
+import React, {useState} from "react";
+import "./SnakeBoard.css";
 
 const SnakeBoard = () => {
-  const height = 10
-  const width = 10
-  var initialRows = []
+  const height = 10;
+  const width = 10;
+  var initialRows = [];
   for (var i = 0; i < height; i++) {
-    initialRows[i] = []
+    initialRows[i] = [];
     for (var j = 0; j < width; j++) {
-      initialRows[i][j] = 'blank'
+      initialRows[i][j] = "blank";
     }
   }
 
-const [rows, setRows] = useState(initialRows)
+  const [rows, setRows] = useState(initialRows);
+  const [snake, setSnake] = useState([{x: 0, y: 0}]);
+  const [direction, setDirection] = useState("right");
 
-const displayRows = rows.map(row =>
-  <div className='Snake-row'>
-    {row.map(tile => <div className={tile} />)}
-  </div>
-)
+  const displayRows = rows.map(row => (
+    <div className="Snake-row">
+      {row.map(tile => (
+        <div className={`tile ${tile}`} />
+      ))}
+    </div>
+  ));
 
-return (
-  <div className='Snake-board'>
-    {displayRows}
-  </div>
-)
-}
+  return <div className="Snake-board">{displayRows}</div>;
+};
 
-export default SnakeBoard
+export default SnakeBoard;
